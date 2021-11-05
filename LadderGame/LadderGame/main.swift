4c31adba97d5de1d9d71409fd1d954008b02997d
bibi <bibi6666667@kakao.com>
2021-11-05 11:27:45 +0900

    사다리 게임 - 사다리 출력하기 구현

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,16 +7,48 @@
 
 import Foundation
 
-print("입력")
+print("참여 인원 입력")
 let input = readLine() ?? ""
 ladderGame(input: Int(input) ?? 0)
 
 func ladderGame(input: Int) {
-    func gameOver() {
+    
+    if input < 2 || input > 8 {
+        printGameOver()
+    } else {
+        printLadder(array: makeLadder(peopleNum: input, ladderHeight: 4))
+    }
+    
+    func printGameOver() {
         print("프로그램 종료")
     }
     
-    if input < 2 || input > 8 {
-        gameOver()
+    func printLadder(array: Array<Array<String>>) {
+        for innerArray in array { // 바깥쪽 배열
+            for item in innerArray { // 안쪽 배열
+                print(item, terminator: "")
+            }
+            print()
+        }
     }
+    
+    func makeLadder(peopleNum: Int, ladderHeight: Int) -> Array<Array<String>> {
+        var resultArray = Array<Array<String>>()
+        for _ in 1...ladderHeight { // 세로
+            var tempArray = Array<String>()
+            for _ in 1...peopleNum-1 { // 가로
+                tempArray.append("|")
+                let randomInt = Int.random(in: 1...100)
+                if randomInt % 2 == 0 {
+                    tempArray.append("-")
+                } else {
+                    tempArray.append(" ")
+                }
+            }
+            tempArray.append("|")
+            resultArray.append(tempArray)
+        }
+        return resultArray
+    }
+
 }
