bc93944a253da19f930f09b7b8e73f8500a4b5e7
bibi <bibi6666667@kakao.com>
2021-11-24 11:35:36 +0900

    day18) 연습 문제

---
+++ day18/day18/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  day18
+//
+//  Created by Bibi on 2021/11/24.
+//
+
+import Foundation
+
+let path = "/Users/bibi/test"
+displayAllFiles(at: path)
+
+
---
+++ day18/day18/practice.swift
@@ -0,0 +1,19 @@
+//
+//  practice.swift
+//  day18
+//
+//  Created by Bibi on 2021/11/24.
+//
+
+import Foundation
+
+func displayAllFiles(at path: String) {
+    let fileManager = FileManager.default
+    
+    do {
+        let contents = try fileManager.contentsOfDirectory(atPath: path)
+        print(contents)
+    } catch let error as NSError {
+        print("Error access directory: \(error)")
+    }
+}
